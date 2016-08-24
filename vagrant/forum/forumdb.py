029d5a0cadcf70a53ba3c266ad49ef1b990092d6
petros <farahpetros+git@gmail.com>
2016-08-24 13:59:38 +0100

    Implemented database backed forum. Includes precautions against common forum databases issues: code-injection and spam

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,23 +1,32 @@
 #
 # Database access functions for the web forum.
 # 
-
-import time
+import psycopg2
+import bleach
 
 ## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
     '''Get all the posts from the database, sorted with the newest first.
+    
 
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    cur = DB.cursor()
+
+    query = "SELECT time,content FROM posts ORDER BY time DESC;"
+    cur.execute(query)
+
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in cur.fetchall()]
+
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +36,14 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    
+    DB = psycopg2.connect("dbname=forum")
+    cur = DB.cursor()
+
+    clean_content = bleach.clean(content)
+    cur.execute("INSERT INTO posts(content) VALUES(%s)",(clean_content,))
+
+    DB.commit()
+    DB.close()
+
+
